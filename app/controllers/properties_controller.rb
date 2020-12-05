4a1a0d3479a5107d2b6c5a65b0409f8e8e8dc96b
wescreations <40309321+wescreations@users.noreply.github.com>
2020-12-05 12:14:59 -0700

    First refactor and styling fix

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,91 +1,80 @@
 class PropertiesController < ApplicationController
-  
 
-before_action :authenticate_user!, :only => [:new, :delete, :destroy, :create, :update, :edit]
+  before_action :authenticate_user!, :only => [:new, :delete, :destroy, :create, :update, :edit]
 
-layout 'application'
+	layout 'application'
 
-  def new
-  	@property = Property.new
-  end
-
-  def create
- 
-  	@property = Property.new(properties_params)
-  	if @property.save
-
-  		flash[:notice] = "Property Created Successfully"
-    	redirect_to(properties_path)
-	else
-		render('new')
+	def new
+		@property = Property.new
 	end
 
-  end
-
-  def edit
-  	@property = Property.find(params[:id])
-  end
-
-  def update
-    @property = Property.find(params[:id])
-    
-  	if @property.update_attributes(properties_params)
-
-  		flash[:notice] = "Property Updated Successfully"
-    	redirect_to(properties_path)
-	else
-		render('new')
+	def create
+		@property = Property.new(properties_params)
+		if @property.save
+			flash[:notice] = "Property Created Successfully!"
+			redirect_to(properties_path)
+		else
+			render('new')
+		end
 	end
-  end
 
-  def index
-    #@property = Property.all
-    @q = Property.ransack(params[:q])
-    @property = @q.result(distinct: true).paginate(page: params[:page]).all
-       
-  end
-
-  def show
-
-  	@property = Property.find(params[:id])
-    @q = Property.ransack(params[:q])
-  	@prop = @q.result(distinct: true).all
-  end
-
-  def delete
-  	@property = Property.find(params[:id])
-  end
-
-  def destroy
-
-  	if @property = Property.destroy(params[:id])
-
-  		flash[:notice] = "Property DESTROYED Successfully"
-    	redirect_to(properties_path)
-	else
-		render('new')
+	def edit
+		@property = Property.find(params[:id])
 	end
-  end
 
-  def properties_params
-    params.require(:property).permit(
-    	:name,  
-    	:price, 
-      :lat, 
-      :long,
-    	:address_1, 
-    	:city, 
-    	:state, 
-    	:zip, 
-      :size,
-    	:apn, 
-    	:monthly_payment, 
-      :price_group, 
-      :acre,
-    	:description, 
-      :main_photo,
-      photos: []
-    )
-  end
+	def update
+		@property = Property.find(params[:id])
+				if @property.update_attributes(properties_params)
+			flash[:notice] = "Property Updated Successfully!"
+			redirect_to(properties_path)
+		else
+			render('new')
+		end
+	end
+
+	def index
+		@q = Property.ransack(params[:q])
+		@property = @q.result(distinct: true).paginate(page: params[:page]).all
+	end
+
+	def show
+		@property = Property.find(params[:id])
+		@q = Property.ransack(params[:q])
+		@prop = @q.result(distinct: true).all
+	end
+
+	def delete
+		@property = Property.find(params[:id])
+	end
+
+	def destroy
+		if @property = Property.destroy(params[:id])
+			flash[:notice] = "Property Destroyed Successfully!"
+			redirect_to(properties_path)
+		else
+			render('new')
+		end
+	end
+
+	def properties_params
+		params.require(:property).permit(
+			:name,
+			:price,
+			:lat,
+			:long,
+			:address_1,
+			:city,
+			:state,
+			:zip,
+			:size,
+			:apn,
+			:monthly_payment,
+			:price_group,
+			:acre,
+			:description,
+			:main_photo,
+			photos: []
+		)
+	end
 
 end
--- app/helpers/properties_helper.rb
+++ app/helpers/properties_helper.rb
@@ -1,10 +1,30 @@
 module PropertiesHelper
-	def property_size(property) 
+	def property_size(property)
 		if @property.size.to_i > 5
 			"LARGE PROPERTY: #{@property.size}"
-		else 	
+		else
 			"NORMAL SIZE PROPERTY: #{@property.size}"
 		end
-  	end
+  end
+
+	def states
+ 		states = [" ", "Arizona","Texass","California", "New Mexico", "Nevada", "New York"]
+	end
+
+	def monthly
+ 		monthly = [" ", "$129 Per Month","$229 Per Month","$329 Per Month", "$429 Per Month"]
+	end
+
+	def price_group
+ 		price_group = [" ", "$0 - $1,000","$1,001 - $2,500","$2,501 - $5,000", "$5,001 - $7,500", "$7,501 - $10,000","$10,001 - $20,000", "$20,001 - $35,000", "$35,001 - $50,000", "$50,001 - $75,000"]
+	end
+
+	def monthly
+ 		monthly = [" ", "$129 Per Month","$229 Per Month","$329 Per Month", "$429 Per Month"]
+	end
+
+	def acre
+		acre = [" ", "0 - .5",".51 - 2","2.01 - 5", "5.01 - 10", "10.01 - 15", "15.01 - 20", "20.01 - 40", "40.01 - 60", "60.01 - 80"]
+	end
 
 end
